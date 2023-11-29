48748ae7e3f0c65a69a6aceacca7f7c8c5c471a5
Victor Timoteo <vtimoteo@ck2.com>
2023-11-29 11:56:39 -0500

    project finished

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -31,16 +31,24 @@ function gymfitness_scripts_styles()
 {
     //Archivos CSS
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
-    wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.4');
-    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.5');
+    if (is_page('galeria')) {
+        wp_enqueue_style('lightboxcss', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.4');
+    }
+    if (is_front_page()) {
+        wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.5');
+    }
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
 
     //Archivos JS
     // wp_enqueue_script('jquery');
-    wp_enqueue_script('lightboxjs', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '2.11.4', true);
-    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.5', true);
+    if (is_page('galeria')) {
+        wp_enqueue_script('lightboxjs', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '2.11.4', true);
+    }
+    if (is_front_page()) {
+        wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.5', true);
+    }
     wp_enqueue_script('anime', 'https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js', array(), '2.0.2', true);
-    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('swiper-js', 'anime'), '11.0.5', true);
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array(), '11.0.5', true);
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
